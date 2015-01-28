c8af18aaded248105178a238f98be41d6413e03e
Quentin McDonald <dqmcdonald@gmail.com>
2015-01-28 15:34:07 +1300

    Multiple notes held, reassign mode buttons
    Now handle multiple notes better using the MIDI library, default
    strategy is last note played wins.
    Revised the way mode buttons work, now one determines the major mode
    and the second changes the values in that mode,

--- MidiToCV.ino
+++ MidiToCV.ino
@@ -2,12 +2,17 @@
 // Generic MIDI to CV converter 
 // Uses DAC module via SPI to convert MIDI events received from
 //
+// Relies of MIDI library from https://github.com/FortySevenEffects/arduino_midi_library/
 
 
 // Note the script needs to include SPI:
 #include <SPI.h>
 #include <DAC.h>
 #include <SoftwareSerial.h>
+#include <MIDI.h>
+#include "noteList.h"
+
+
 
 #define RX_PIN 2
 #define TX_PIN 3
@@ -15,7 +20,7 @@ SoftwareSerial ss =  SoftwareSerial( RX_PIN, TX_PIN );
 
 #define GATE_PIN 5
 
-#define LOWEST_NOTE  0x24
+#define DEFAULT_LOWEST_NOTE  0x24
 
 #define NOTE_OFF   0x80 
 #define NOTE_ON    0x90
@@ -26,6 +31,15 @@ SoftwareSerial ss =  SoftwareSerial( RX_PIN, TX_PIN );
 #define GLIDE_CONTROLLER 0x52
 
 
+#define MODE_OUTPUT      0
+#define MODE_CHANNEL     1
+#define MODE_GLIDE       2
+#define MODE_LOWEST_NOTE 3
+#define MODE_LEGATO      4
+
+#define MAX_MODE 4
+
+
 #define MODE_AB 2      // Note to A + B
 #define MODE_APBE 0    // Note to A, Mod wheel to B
 #define MODE_APBV 1    // Note to A, Velocity to B
@@ -33,11 +47,11 @@ SoftwareSerial ss =  SoftwareSerial( RX_PIN, TX_PIN );
 #define MODE_TUNEA220 4
 #define MODE_TUNEA880 5
 
-#define MAX_MODE 5  // The maximum number of modes
+#define MAX_OUTPUT_MODE 5  // The maximum number of modes
 
 
-#define PITCH_WHEEL_CENTERED 0x2000
-#define PITCH_WHEEL_MAX 0x3FFF
+#define PITCH_WHEEL_CENTERED MIDI_PITCHBEND_MAX/2
+
 #define MOD_WHEEL_MAX 0xFF
 
 //#define LCD_DEBUGGING 1
@@ -61,7 +75,8 @@ private:
 
 SerialLCD lcd;
 
-
+const int LOWEST_NOTES[] = { 
+  12, 24, 36, 48, 60 };
 
 const float SEMITONE_INC = 1.0/12.0;  // semitone increment
 
@@ -71,8 +86,8 @@ float note_voltage = 0.0;
 #define KNOB1  0
 #define KNOB2  1
 
-#define BUTTON1  17
-#define BUTTON2  18
+#define BUTTON1  17   // Button 1 is the "Mode" button
+#define BUTTON2  18   // Button 2 is the "Value" button
 #define GATE_LED_PIN 16
 
 #define STAT1  7
@@ -91,7 +106,7 @@ float note_voltage = 0.0;
 #define SHDN_PIN 9
 #define SS_PIN  10
 
-
+int lowest_note_index = 1;
 int total_bytes =0; // The total number of MIDI bytes received
 int disp_line = 1;
 byte note;
@@ -102,7 +117,6 @@ byte stat;
 byte current_channel = 1;
 byte current_note = -1;
 char buff[21];
-byte midi_data[3];
 int num_bytes_expected = 0;
 int cnt=0;
 int button1_press = 0;
@@ -114,9 +128,15 @@ bool doing_glide = false; // A flag to indicate we are gliding to a new value
 unsigned long glide_target; // The time in millis() we expect to have eached the note target:
 float glide_target_voltage = 0.0; // The voltage we want to reach
 float glide_start_voltage = 0.0;  // The voltage we started at
+static const unsigned int MAX_NUM_NOTES = 16; // Maximum number of MIDI notes
+MidiNoteList<MAX_NUM_NOTES> midi_notes;
+
+int current_mode = MODE_OUTPUT;  // The current input and display mode
+
+bool legato_mode = false;
 
 
-
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial, MIDI);
 
 
 
@@ -162,10 +182,17 @@ void setup() {
   lcd.backlight(20);
   digitalWrite( GATE_LED_PIN, HIGH );
   lcd.displayLine( 1, "MIDI2CV Conv.   ");
-  lcd.displayLine( 2, "Version 1.0     ");
+  lcd.displayLine( 2, "Version 1.1     ");
   delay( 5000 );
   digitalWrite( GATE_LED_PIN, LOW );
   update_lcd();
+
+  MIDI.setHandleNoteOn(handle_note_on);  
+  MIDI.setHandleNoteOff(handle_note_off);
+  MIDI.setHandlePitchBend(handle_pitch_bend);
+  MIDI.setHandleControlChange(handle_control_change);
+  // Initiate MIDI communications, listen to current channel
+  MIDI.begin(current_channel);
 
 
 
@@ -184,149 +211,76 @@ void loop() {
 
   update_glide_voltage();
 
-
-  if( button( BUTTON1 ) ) {
-    delay(100);
-    if( button( BUTTON1 ) ) {
-      current_channel += 1;
-      if( current_channel > 12 ) {
-        current_channel = 1;
-      }
-      update_lcd();
-      while( button( BUTTON1 ) ) {
-      }
-    }
-  }
-
-  if( button( BUTTON2 ) ) {
-    delay(100);
-    if( button( BUTTON2 ) ) {
-      out_mode += 1;
-      if( out_mode > MAX_MODE) {
-        out_mode = 0;
-      }
-      update_lcd();
-      while( button( BUTTON2 ) ) {
-      }
-      if( out_mode == MODE_TUNEA440 ) {
-        note_on( A440, 128 );
-      } 
-      else if( out_mode == MODE_TUNEA220 ) {
-        note_on( A220, 128 );
-      } 
-      else if( out_mode == MODE_TUNEA880 ) {
-        note_on( A880, 128 );
-      } 
-      else {
-        note_off( A440 );
-        current_note = -1;
-      }
+  handle_button_one();
+  handle_button_two();
 
 
-    }
-  }
 
 
 
   //*************** MIDI IN ***************//
-  if (Serial.available() > 0) {
+  // Call MIDI.read the fastest you can for real-time performance.
+  MIDI.read();
 
-    // read the incoming byte:
-    midi_data[cnt] = Serial.read();
-    total_bytes += 1;
+}
 
-
-#ifdef LCD_DEBUGGING
-    snprintf(buff, 16, "%2d: %2x H%2x L%2x", total_bytes, midi_data[cnt], midi_data[cnt]&0xF0, midi_data[cnt]&0x0F );
-
-    lcd.displayLine( disp_line, buff );
-    disp_line += 1;
-    if( disp_line > 2)
-      disp_line = 1;
-#endif
-
-    cnt += 1;
-
-    if( cnt == 1 ) {     
-
-
-      stat = byte( midi_data[0] & 0xF0);
-      channel = byte( 1 + midi_data[0] & 0x0F) ;
-
-
-      if( stat == NOTE_ON ) {
-        num_bytes_expected = 3;
+void handle_notes_changed(bool is_first_note = false)
+{
+  if (midi_notes.empty())
+  // No notes - turn the gate off:
+  {
+    set_gate(false);
+  }
+  else
+  {
+    // Possible playing modes:
+    // Mono Low:  use midi_notes.getLow
+    // Mono High: use midi_notes.getHigh
+    // Mono Last: use midi_notes.getLast
+    byte note = 0;
+    byte vel = 0;
+    if (midi_notes.getLast(note,vel))
+    {
+      set_voltage( note, vel );
+      if (is_first_note)
+      {
+        set_gate(true);
       }
-      else if( stat == NOTE_OFF ) {
-        num_bytes_expected = 3; 
-      }
-      else if( stat == PITCH_BEND ) {
-        num_bytes_expected = 3;
-      }
-      else if( stat == PROG_CHANGE ) {
-        num_bytes_expected = 2;
-      }
-      else {
-        num_bytes_expected = 3;
+      else
+      {
+        if( !legato_mode ) 
+          pulse_gate(); // Re trigger envelopes. Remove for legato effect.
       }
     }
-
-    // Once we have the reqired amount of data and on the right channel then action the note:
-    if( cnt == num_bytes_expected ) {
-      cnt = 0;
-
-
-      if( channel == current_channel) {
-
-        if( stat == NOTE_ON && midi_data[2] != 0 ) {
-
-          note_on(midi_data[1], midi_data[2]);
-          current_note = midi_data[1];
-        }
-
-        if( stat == NOTE_OFF || (stat == NOTE_ON && midi_data[2] == 0 )) {
-          if( current_note == midi_data[1] ) {
-            note_off( midi_data[1]); 
-            current_note = -1;
-          }
-        }
-        if( stat == PITCH_BEND ) {
-          handle_pitch_bend();
-
-        }
-        if( stat == CONTROL_CHANGE ) {
-          if( midi_data[1] == MOD_WHEEL ) {
-            handle_mod_wheel();
-          }
-          if( midi_data[1] == GLIDE_CONTROLLER ) {
-            handle_glide_control(); 
-          }
-        }
-
-      }
-    }
-
   }
 }
 
+
+
 // Handle a note on event. Set the voltage and turn the GATE on:
-void note_on( byte note, byte velocity)
+void handle_note_on( byte channel, byte note, byte velocity)
 {
+
+  const bool first_note = midi_notes.empty();
+  midi_notes.add(MidiNote(note, velocity));
+  handle_notes_changed(first_note);
+}
+
+
+void set_voltage( byte note, byte velocity ) {
+
+
   float vel_voltage;
-  digitalWrite(GATE_PIN, LOW ); // Turn the gate off
-  digitalWrite( GATE_LED_PIN, LOW );
   glide_start_voltage = note_voltage;
 
-  note_voltage = (float)(note - (byte)LOWEST_NOTE ) * SEMITONE_INC;  
+  note_voltage = (float)(note - (byte)LOWEST_NOTES[lowest_note_index] ) 
+    * SEMITONE_INC;  
   if( note_voltage < 0.0 ) {
     note_voltage = 0.0; 
   }
   if( note_voltage > 5.0 ) {
     note_voltage = 5.0; 
   }
-  digitalWrite(STAT2, LOW);
-  digitalWrite(GATE_PIN, HIGH );
-  digitalWrite( GATE_LED_PIN, HIGH );
 
   if( glide_value > 0 ) {
     doing_glide = true;
@@ -357,12 +311,11 @@ void note_on( byte note, byte velocity)
 }
 
 
-// Handle a note-off event. Simply turn the gate off:
-void note_off( byte note ) {
-  digitalWrite( GATE_PIN, LOW ); 
-  digitalWrite( GATE_LED_PIN, LOW );
-  digitalWrite( STAT2, HIGH );
-
+// Handle a note-off event.
+void handle_note_off(byte channel, byte pitch, byte velocity ) {
+  
+    midi_notes.remove(pitch);
+    handle_notes_changed();
 }
 
 char button(char button_num)
@@ -370,36 +323,173 @@ char button(char button_num)
   return (!(digitalRead(button_num)));
 }
 
-void update_lcd( void ) {
+void handle_button_one ( void ) {
+  // The mode button - simply update the main mode and redisplay in the LCD
+  //
+  if( button( BUTTON1 ) ) {
+    delay(100);
+    if( button( BUTTON1 ) ) {
+      current_mode += 1;
+      if( current_mode > MAX_MODE ) {
+        current_mode = 0;
+      }
+      update_lcd();
+      while( button( BUTTON1 ) ) {
 
-  snprintf(buff, 16, "Chn:%2d Gld:%4d", int(current_channel), int(glide_value) );
-  lcd.displayLine( 1, buff );
+      }
 
-  if( out_mode == MODE_AB ) {
-    lcd.displayLine( 2, "Mode:A+B" );
-  } 
-  else if (out_mode == MODE_APBE ) {
-    lcd.displayLine( 2, "Mode:A=Pth,B=Exp");
-  } 
-  else if (out_mode == MODE_APBV ) {
-    lcd.displayLine( 2, "Mode:A=Pth,B=Vel");
-  } 
-  else if (out_mode == MODE_TUNEA440 ) {
-    lcd.displayLine( 2, "Mode:Tune 440Hz ");
+    }
   }
-  else if (out_mode == MODE_TUNEA220 ) {
-    lcd.displayLine( 2, "Mode:Tune 220Hz ");
-  }
-  else if (out_mode == MODE_TUNEA880 ) {
-    lcd.displayLine( 2, "Mode:Tune 880Hz ");
-  }
+
 }
 
-void handle_pitch_bend( void ) {
-  pitch_bend_value = midi_data[2];
-  pitch_bend_value = pitch_bend_value << 7;
-  pitch_bend_value = pitch_bend_value | midi_data[1];
-  float voltage_offset = float(pitch_bend_value-PITCH_WHEEL_CENTERED) /PITCH_WHEEL_MAX * SEMITONE_INC * 4;
+void handle_button_two ( void ) {
+  // The value button - what we do here depends on the value of the main mode:
+
+
+
+  if( button( BUTTON2 ) ) {
+    delay(100);
+    if( button( BUTTON2 ) ) {
+
+
+      if( current_mode == MODE_OUTPUT) {
+        out_mode += 1;
+        if( out_mode > MAX_OUTPUT_MODE) {
+          out_mode = 0;
+        }
+
+
+        if( out_mode == MODE_TUNEA440 ) {
+          handle_note_on( current_channel, A440, 128 );
+        } 
+        else if( out_mode == MODE_TUNEA220 ) {
+          handle_note_on( current_channel, A220, 128 );
+        } 
+        else if( out_mode == MODE_TUNEA880 ) {
+          handle_note_on( current_channel, A880, 128 );
+        } 
+        else {
+          handle_note_off( current_channel, A440, 0 );
+          current_note = -1;
+        }
+
+      }
+    }
+
+    if( current_mode == MODE_CHANNEL ) {
+      current_channel += 1;
+      if( current_channel > 12 ) {
+        current_channel = 0;
+      }
+      MIDI.setInputChannel( current_channel );
+
+    }
+
+    if( current_mode == MODE_GLIDE) {
+      glide_value += 5;
+      if( glide_value > 127 ) {
+        glide_value = 0;
+      }
+    }
+
+    if( current_mode == MODE_LOWEST_NOTE ) {
+      lowest_note_index += 1;
+      if( lowest_note_index > 4 ) {
+        lowest_note_index = 0;
+      }
+    }
+
+    if( current_mode == MODE_LEGATO ) {
+      legato_mode = ! legato_mode;
+    }
+
+
+
+  }
+
+
+  update_lcd();
+  while( button( BUTTON2 ) ) {
+  }
+
+}
+
+void update_lcd( void ) {
+
+
+  // In ouput mode
+  if( current_mode == MODE_OUTPUT ) {
+    lcd.displayLine( 1, "Output Mode:" );
+    if( out_mode == MODE_AB ) {
+      lcd.displayLine( 2, "Pitch to A + B" );
+    } 
+    else if (out_mode == MODE_APBE ) {
+      lcd.displayLine( 2, "Pitch: A, Exp: B");
+    } 
+    else if (out_mode == MODE_APBV ) {
+      lcd.displayLine( 2, "Pitch: A, Vel: B");
+    } 
+    else if (out_mode == MODE_TUNEA440 ) {
+      lcd.displayLine( 2, "Tune 440Hz ");
+    }
+    else if (out_mode == MODE_TUNEA220 ) {
+      lcd.displayLine( 2, "Tune 220Hz ");
+    }
+    else if (out_mode == MODE_TUNEA880 ) {
+      lcd.displayLine( 2, "Tune 880Hz ");
+    }
+  }
+
+  if( current_mode == MODE_CHANNEL ) {
+    snprintf(buff, 16, "Channel :%2d", int(current_channel) );
+    lcd.displayLine( 1, buff );
+    lcd.displayLine( 2, "" );
+  }
+
+  if( current_mode == MODE_GLIDE ) {
+    snprintf(buff, 16, "Glide: %4d", int(glide_value) );
+    lcd.displayLine( 1, buff );
+    lcd.displayLine( 2, "" );
+  }
+
+  if( current_mode == MODE_LOWEST_NOTE ) {
+    snprintf(buff, 16, "Low Note: C%1d", int(lowest_note_index) );
+    lcd.displayLine( 1, buff );
+    lcd.displayLine( 2, "" );
+  }
+
+  if( current_mode == MODE_LEGATO ) {
+    if( legato_mode )
+      snprintf(buff, 16, "Legato On");
+    else
+      snprintf(buff, 16, "Legato off");
+    lcd.displayLine( 1, buff );
+    lcd.displayLine( 2, "" );
+  }
+
+
+
+}
+
+inline void set_gate(bool gate_active)
+{
+  digitalWrite(GATE_PIN, gate_active ? HIGH : LOW);
+  digitalWrite(GATE_LED_PIN, gate_active ? HIGH : LOW);
+}
+
+inline void pulse_gate()
+{
+  set_gate(false);
+  delay(1);
+  set_gate(true);
+}
+
+
+
+
+void handle_pitch_bend( byte channel, int bend  ) {
+  float voltage_offset = float(bend-PITCH_WHEEL_CENTERED) /MIDI_PITCHBEND_MAX * SEMITONE_INC * 4;
   float voltage = note_voltage + voltage_offset;
   if( voltage < 0.0 )
     voltage = 0.0;
@@ -413,9 +503,20 @@ void handle_pitch_bend( void ) {
   // lcd.displayLine( 2, buff );
 }
 
-void handle_mod_wheel( void ) {
+void handle_control_change( byte channel, byte number, byte value  ) {
 
-  float voltage = (float(midi_data[2])/MOD_WHEEL_MAX)*10.0;
+  if( number == MOD_WHEEL ) {
+    handle_mod_wheel(value);
+  }
+  if( number == GLIDE_CONTROLLER ) {
+    handle_glide_control(value); 
+  }
+}
+
+
+void handle_mod_wheel( byte value ) {
+
+  float voltage = (float(value)/MOD_WHEEL_MAX)*10.0;
 
   if( out_mode == MODE_APBE ) {
     // snprintf( buff, 16, "MW: %d",int(voltage*100) );
@@ -430,8 +531,8 @@ void handle_mod_wheel( void ) {
 
 }
 
-void handle_glide_control( void ) {
-  int glide = int((float(midi_data[2])/127)*1000);
+void handle_glide_control( byte value ) {
+  int glide = int((float(value)/127)*1000);
   glide_value = glide;
   update_lcd();
   doing_glide= false; // turn off the current glide
@@ -458,6 +559,17 @@ void update_glide_voltage( void ) {
     dac.setVoltage( note_voltage, CHANNEL_B );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
